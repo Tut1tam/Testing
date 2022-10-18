1aa15b86dad108e997efc9900e5687991eaafe8c
Tut1tam <dolzhich.maxim@yandex.ru>
2022-10-18 23:24:35 +0300

    edit integral_test.py

--- integral_test.py
+++ integral_test.py
@@ -7,8 +7,9 @@ class TestIntegral(unittest.TestCase):
     # Тест кейс на решение интегралов
     def test__integral(self):
         self.integral = Integral(5, 0, 10)
+        res, x = 100.0, 5
         self.assertEqual(self.integral.draw_integral(), f'Ваш интеграл - ∫(x + 5)dx, с границами от 0 до 10')
-        self.assertEqual(self.integral.calc_integral(), f'Результат вычисления интеграла ∫(x + 5)dx = 100.0\n')        
+        self.assertEqual(self.integral.calc_integral(), f'Результат вычисления интеграла ∫(x + {x})dx = {res}')        
 
 
 if __name__ == '__main__':
